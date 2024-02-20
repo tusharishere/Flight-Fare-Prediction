454d4a09daaf489cc8c3ccdd492e7e569c825a42
tusharishere <tusharsinha1452@gmail.com>
2024-02-20 15:48:32 +0530

    create environment

---
+++ init_setup.sh
@@ -0,0 +1,26 @@
+echo [$(date)]: "START"
+
+echo [$(date)]: "Creating conda env with Python 3.9 version"
+
+conda create -p ./env python=3.9 -y
+# python -m venv .venv
+
+echo [$(date)]: "Activating the environment"
+
+# source .venv/Scripts/activate
+# conda update conda
+
+source activate venv
+# source activate venv
+
+echo [$(date)]: "Installing the dev requirements"
+
+python -m pip install --upgrade pip
+
+pip install -r requirements.txt 
+
+echo [$(date)]: "Installing the project as local package"
+
+# python setup.py install
+
+echo [$(date)]: "END"
